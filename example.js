5d40446da55f3fdbbf4c8ff14427d9de92ce07d8
JeonYooDeok <60500298+JeonYooDeok@users.noreply.github.com>
2021-09-17 18:13:35 +0900

    Add files via upload

--- example.js
+++ example.js
@@ -4,6 +4,9 @@ if (typeof window.ethereum !== 'undefined') {
 
 const ethereumButton = document.querySelector('.connetMetamask');
 const myAccount = document.querySelector('.myAccount');
+const requestPermissionsButton = document.getElementById('requestPermissions')
+const getPermissionsButton = document.getElementById('getPermissions')
+const permissionsResult = document.getElementById('permissionsResult')
 
 ethereumButton.addEventListener('click', () => {
     getAccount();
@@ -15,4 +18,44 @@ async function getAccount() {
     });
     const account = accounts[0];
     myAccount.innerHTML = account;
+}
+
+
+requestPermissionsButton.onclick = async () => {
+    try {
+        const permissionsArray = await ethereum.request({
+            method: 'wallet_requestPermissions',
+            params: [{
+                eth_accounts: {}
+            }],
+        })
+        permissionsResult.innerHTML = getPermissionsDisplayString(permissionsArray)
+    } catch (err) {
+        console.error(err)
+        permissionsResult.innerHTML = `Error: ${err.message}`
+    }
+}
+
+getPermissionsButton.onclick = async () => {
+    try {
+        const permissionsArray = await ethereum.request({
+            method: 'wallet_getPermissions',
+        })
+        permissionsResult.innerHTML = getPermissionsDisplayString(permissionsArray)
+    } catch (err) {
+        console.error(err)
+        permissionsResult.innerHTML = `Error: ${err.message}`
+    }
+}
+
+getAccountsButton.onclick = async () => {
+    try {
+        const _accounts = await ethereum.request({
+            method: 'eth_accounts',
+        })
+        getAccountsResults.innerHTML = _accounts[0] || 'Not able to get accounts'
+    } catch (err) {
+        console.error(err)
+        getAccountsResults.innerHTML = `Error: ${err.message}`
+    }
 }
